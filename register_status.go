64bfc9c68677022f9b819c9ad4d255849724871f
Miguel Cabrerizo <doncicuto@gmail.com>
2025-11-21 18:13:06 +0100

    feat: new register status send cert

--- register_status.go
+++ register_status.go
@@ -7,6 +7,7 @@ const REGISTER_REVOKED = "users.certificate_revoked"
 const REGISTER_APPROVED = "users.approved"
 const REGISTER_PASSWORD_LINK_SENT = "users.password_link_sent"
 const REGISTER_OIDC_FIRST_LOGIN = "users.oidc_first_login"
+const REGISTER_SEND_CERTIFICATE = "users.send_certificate"
 
 func RegisterPossibleStatus() []string {
 	return []string{REGISTER_CERTIFICATE_SENT, REGISTER_COMPLETE, REGISTER_IN_REVIEW}
