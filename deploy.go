cfc4d493d693ba772f34dce8f5ced4d827dfcf51
Miguel Cabrerizo <doncicuto@gmail.com>
2026-03-27 09:40:43 +0100

    fix: add package branch and brew type

--- deploy.go
+++ deploy.go
@@ -3,13 +3,15 @@ package nats
 import "time"
 
 type DeployAction struct {
-	AgentId        string    `json:"agentid,omitempty"`
-	Action         string    `json:"action,omitempty"`
-	When           time.Time `json:"when,omitempty"`
-	PackageId      string    `json:"packageid,omitempty"`
-	PackageName    string    `json:"packagename,omitempty"`
-	PackageVersion string    `json:"packageversion,omitempty"`
-	Repository     string    `json:"repository,omitempty"`
-	Info           string    `json:"info,omitempty"`
-	Failed         bool      `json:"failed,omitempty"`
+	AgentId         string    `json:"agentid,omitempty"`
+	Action          string    `json:"action,omitempty"`
+	When            time.Time `json:"when,omitempty"`
+	PackageId       string    `json:"packageid,omitempty"`
+	PackageName     string    `json:"packagename,omitempty"`
+	PackageVersion  string    `json:"packageversion,omitempty"`
+	PackageBranch   string    `json:"packagebranch,omitempty"`
+	PackageBrewType string    `json:"packagebrewtype,omitempty"`
+	Repository      string    `json:"repository,omitempty"`
+	Info            string    `json:"info,omitempty"`
+	Failed          bool      `json:"failed,omitempty"`
 }
